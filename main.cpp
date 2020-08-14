af2026cd8893f66140357f9d58e27d0d41bbd545
OlyaKnyaz-Labs <Gfhjkm-2019>
2020-08-14 16:44:14 +0700

    Func quiz added (also tails for others)
    Signed-off-by: OlyaKnyaz-Labs <Gfhjkm-2019>

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int InputCheck(const char *in_line) {
     int ch_num = atoi(in_line);
     return (ch_num);
-};
+}
 int LevelStartMenu(int in_num) {
     int lvl;
     switch (in_num) {
@@ -14,7 +14,7 @@ int LevelStartMenu(int in_num) {
             lvl = in_num;
             break;
         case 2:
-            cout << "Синтаксис ";
+            cout << "Синтаксис(Найдите ошибку в кусочке кода)";
             lvl = in_num;
             break;
         case 3:
@@ -33,14 +33,14 @@ int LevelStartMenu(int in_num) {
 }
 
 
-int Comparse(string in, string check) {
+int Comparse(const string &in, const string &check) {
     if (in == check) {
         return (1);
     } else {
         return (0);
     }
 }
-int Ask(string quest, string answer) {
+int Ask(const string &quest, const string &answer) {
     cout << endl
          << "____________________" << endl
          << quest << endl
@@ -57,6 +57,7 @@ int Ask(string quest, string answer) {
     }
 }
 int Quiz(const string &path1, const string &path2) {
+    int points = 0;
     int size = 8;
     string quiz_src[size], quiz_answer[size], temp;
     //ЗАПОЛНЕНИЕ ПУЛА ВОПРОСОВ
@@ -82,43 +83,68 @@ int Quiz(const string &path1, const string &path2) {
      }*/
     for (int j = 0; j < 8; j++) {
 
-        Ask(quiz_src[j], quiz_answer[j]);
+        points += Ask(quiz_src[j], quiz_answer[j]);
     }
+    return (points);
 }
 int StartMenuDrop() {
-    cout << "+-----------------------------------------------+\n"
-            "|            QuizRunner's Test system           |\n"
-            "|                                               |\n"
-            "|                      С++                      |\n"
-            "|                                               |\n"
-            "+-----------------------------------------------+\n"
-            "|Переменные             |Синтаксис              |\n"
-            "|                       |                       |\n"
-            "+-----------------------------------------------+\n"
-            "|Верно /неверно         |Микс                   |\n"
-            "|                       |                       |\n"
-            "+-----------------------+-----------------------+\n";
 
     string key_in;
     int lvl = 0;
     while (lvl == 0) {
         cin >> key_in;
         int key_formatted = InputCheck(key_in.c_str());
-        ;
+
         cout << "Выбрано " << key_formatted << " . Это соответствует ";
         lvl = LevelStartMenu(key_formatted);
     }
     return (lvl);
 }
 int main() {
-    int lvl_token = StartMenuDrop();
-    switch (lvl_token) {
+    int point_pool[4];
+    for (int j = 0; j < 4; j++) {
+        point_pool[j] = 0;
+    }
+    while (1) {
+        cout << "+-----------------------------------------------+\n"
+                "|            QuizRunner's Test system           |\n"
+                "|                                               |\n"
+                "|                      С++                      |\n"
+                "|                    СУММА - "
+             << point_pool[0] + point_pool[1] + point_pool[3] + point_pool[2] << "                  |\n"
+                                                                                 "+-----------------------------------------------+\n"
+                                                                                 "|Переменные -1          |Функции  -2            |\n"
+                                                                                 "|Баллов-"
+             << point_pool[0] << "               |Баллов-" << point_pool[1] << "               |\n"
+                                                                               "+-----------------------------------------------+\n"
+                                                                               "|Верно /неверно -3      |Микс  -4               |\n"
+                                                                               "|Баллов-"
+             << point_pool[2] << "               | Баллов-" << point_pool[3] << "              |\n"
+                                                                                "+-----------------------+-----------------------+\n";
 
-        case 1:
-            Quiz("QuizSource_Var.txt", "QuizSource_Var_answer.txt");
-            break;
-        default:
-            exit(2);
+        int lvl_token = StartMenuDrop();
+
+        switch (lvl_token) {
+
+            case 1:
+                point_pool[lvl_token - 1] = Quiz("1.txt", "1_a.txt");
+                cout << "Отвечено верно " << point_pool[lvl_token - 1] << " из 8\n";
+                break;
+            case 2:
+                point_pool[lvl_token - 1] = Quiz("2.txt", "2_a.txt");
+                cout << "Отвечено верно " << point_pool[lvl_token - 1] << " из 8\n";
+                break;
+            case 3:
+                point_pool[lvl_token - 1] = Quiz("3.txt", "3_a.txt");
+                cout << "Отвечено верно " << point_pool[lvl_token - 1] << " из 8\n";
+                break;
+            case 4:
+                point_pool[lvl_token - 1] = Quiz("4.txt", "4_a.txt");
+                cout << "Отвечено верно " << point_pool[lvl_token - 1] << " из 8\n";
+                break;
+            default:
+                exit(2);
+        }
     }
     return 0;
 }
