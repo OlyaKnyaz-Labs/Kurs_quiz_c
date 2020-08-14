b889a59a7272c27105ff02aa2a68cf04db325780
OlyaKnyaz-Labs <Gfhjkm-2019>
2020-08-14 14:04:39 +0700

    LevelStartMenu func add
    Signed-off-by: OlyaKnyaz-Labs <Gfhjkm-2019>

--- main.cpp
+++ main.cpp
@@ -5,6 +5,31 @@ int InputCheck(const char *in_line) {
     int ch_num = atoi(in_line);
     return (ch_num);
 };
+int LevelStartMenu(int in_num) {
+    int lvl;
+    switch (in_num) {
+        case 1:
+            cout << "Переменные";
+            lvl = in_num;
+            break;
+        case 2:
+            cout << "Синтаксис";
+            lvl = in_num;
+            break;
+        case 3:
+            cout << "Верно /неверно";
+            lvl = in_num;
+            break;
+        case 4:
+            cout << "Микс  ";
+            lvl = in_num;
+            break;
+        default:
+            cout << "Ничему,введите значение еще раз";
+            lvl = 0;
+    }
+    return (lvl);
+}
 int StartMenuDrop() {
     cout << "+-----------------------------------------------+\n"
             "|            QuizRunner's Test system           |\n"
@@ -20,9 +45,11 @@ int StartMenuDrop() {
             "+-----------------------+-----------------------+\n";
 
     string key_in;
-    cin >> key_in;
-    InputCheck(key_in.c_str());
-    cout << InputCheck(key_in.c_str());
+    while (1) {
+        cin >> key_in;
+        InputCheck(key_in.c_str());
+        cout << "Выбрано " << InputCheck(key_in.c_str()) << " . Это соответствует ";
+    }
 }
 int main() {
     StartMenuDrop();
