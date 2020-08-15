1da50055ea2188b1fe23a12a813193c4504bc128
OlyaKnyaz-Labs <Gfhjkm-2019>
2020-08-15 16:13:22 +0700

    format
    Signed-off-by: OlyaKnyaz-Labs <Gfhjkm-2019>

--- src/Check.cpp
+++ src/Check.cpp
@@ -1,12 +1,9 @@
 #include "Check.h"
 #include <iostream>
-
-#ifndef KEYBOARD_NINJA_INPUTCHECK_H
-#define KEYBOARD_NINJA_INPUTCHECK_H
 #include <string>
-int InputCheck(std::string getLevelNumber) {
+int InputCheck(std::string getLevelNumber)
+{
     int Number = 0;
-    Number = atoi(getLevelNumber.c_str());//
+    Number = atoi(getLevelNumber.c_str()); //
     return Number;
 }
-#endif// KEYBOARD_NINJA_INPUTCHECK_H
