1006cebd5976e29a2c0b2dbe0adcec5b551198d4
OlyaKnyaz-Labs <Gfhjkm-2019>
2020-08-15 15:59:33 +0700

    all tests
    Signed-off-by: OlyaKnyaz-Labs <Gfhjkm-2019>

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -7,23 +7,18 @@ int LevelStartMenu(int in_num) {
     //ВЫБОР ПУНКТА МЕНЮ
     switch (in_num) {
         case 1:
-            cout << "Переменные ";
             lvl = in_num;
             break;
         case 2:
-            cout << "Функции";
             lvl = in_num;
             break;
         case 3:
-            cout << "Верно /неверно ";
             lvl = in_num;
             break;
         case 4:
-            cout << "Микс  ";
             lvl = in_num;
             break;
         default:
-            cout << "Ничему,введите значение еще раз ";
             lvl = 0;
     }
     return (lvl);
@@ -33,6 +28,7 @@ int StartMenuDrop() {
     string key_in;
     int lvl = 0;
     while (lvl == 0) {
+        cout << "\nВведите корректное значение (1-4)\n";
         cin >> key_in;
         if (key_in == "exit") {
             exit(0);
@@ -42,7 +38,7 @@ int StartMenuDrop() {
             exit(0);
         }
 
-        cout << "Выбрано " << key_formatted << " . Это соответствует ";
+        cout << "Выбрано " << key_formatted;
         lvl = LevelStartMenu(key_formatted);
     }
     return (lvl);
--- tests/Test.cpp
+++ tests/Test.cpp
@@ -1,4 +1,6 @@
+#include "../src/Body.h"
 #include "../src/Check.h"
+#include "../src/Menu.h"
 #include "gtest/gtest.h"
 
 TEST(InputCheckTest, Input) {
@@ -9,4 +11,24 @@ TEST(InputCheckTest, An_Input) {
 }
 TEST(InputCheckTest, Wr_Input) {
     ASSERT_EQ(InputCheck("e"), 0);
+}
+
+TEST(ComparseTest, Input) {
+    ASSERT_EQ(Comparse("1", "1"), 1);
+}
+TEST(ComparseTest, An_Input) {
+    ASSERT_EQ(Comparse("3", "3"), 1);
+}
+TEST(ComparseTest, Wr_Input) {
+    ASSERT_EQ(Comparse("e", "4"), 0);
+}
+// LevelStartMenu(int in_num)
+TEST(LevelStartMenuTest, Input) {
+    ASSERT_EQ(LevelStartMenu(1), 1);
+}
+TEST(LevelStartMenuTest, An_Input) {
+    ASSERT_EQ(LevelStartMenu(2), 2);
+}
+TEST(LevelStartMenuTest, Wr_Input) {
+    ASSERT_EQ(LevelStartMenu(1515), 0);
 }
