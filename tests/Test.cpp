95df96f3314a7184ce0b8e8dd08488e571fb98b5
OlyaKnyaz-Labs <Gfhjkm-2019>
2020-08-15 15:41:17 +0700

    Check.h test succ
    Signed-off-by: OlyaKnyaz-Labs <Gfhjkm-2019>

---
+++ tests/Test.cpp
@@ -0,0 +1,12 @@
+#include "../src/Check.h"
+#include "gtest/gtest.h"
+
+TEST(InputCheckTest, Input) {
+    ASSERT_EQ(InputCheck("1"), 1);
+}
+TEST(InputCheckTest, An_Input) {
+    ASSERT_EQ(InputCheck("3"), 3);
+}
+TEST(InputCheckTest, Wr_Input) {
+    ASSERT_EQ(InputCheck("e"), 0);
+}
---
+++ tests/install.sh
@@ -0,0 +1,4 @@
+echo "Compiling gtests ..."
+g++ -Wall -Werror -std=c++1z -Igoogletest/include -Igoogletest -Lgoogletest/lib -o Gtests.exe \
+	googletest/lib/libgtest.a \
+	googletest/src/gtest-all.cc main.cpp Test.cpp ../src/Check.h ../src/Check.cpp -pthread
