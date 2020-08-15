1dd1b2c8ca0264c3c566962c3ef4ad45d55e7bbd
OlyaKnyaz-Labs <Gfhjkm-2019>
2020-08-15 16:16:59 +0700

    add test dir to travis
    Signed-off-by: OlyaKnyaz-Labs <Gfhjkm-2019>

--- tests/Test.cpp
+++ tests/Test.cpp
@@ -3,32 +3,41 @@
 #include "../src/Menu.h"
 #include "gtest/gtest.h"
 
-TEST(InputCheckTest, Input) {
+TEST(InputCheckTest, Input)
+{
     ASSERT_EQ(InputCheck("1"), 1);
 }
-TEST(InputCheckTest, An_Input) {
+TEST(InputCheckTest, An_Input)
+{
     ASSERT_EQ(InputCheck("3"), 3);
 }
-TEST(InputCheckTest, Wr_Input) {
+TEST(InputCheckTest, Wr_Input)
+{
     ASSERT_EQ(InputCheck("e"), 0);
 }
 
-TEST(ComparseTest, Input) {
+TEST(ComparseTest, Input)
+{
     ASSERT_EQ(Comparse("1", "1"), 1);
 }
-TEST(ComparseTest, An_Input) {
+TEST(ComparseTest, An_Input)
+{
     ASSERT_EQ(Comparse("3", "3"), 1);
 }
-TEST(ComparseTest, Wr_Input) {
+TEST(ComparseTest, Wr_Input)
+{
     ASSERT_EQ(Comparse("e", "4"), 0);
 }
 // LevelStartMenu(int in_num)
-TEST(LevelStartMenuTest, Input) {
+TEST(LevelStartMenuTest, Input)
+{
     ASSERT_EQ(LevelStartMenu(1), 1);
 }
-TEST(LevelStartMenuTest, An_Input) {
+TEST(LevelStartMenuTest, An_Input)
+{
     ASSERT_EQ(LevelStartMenu(2), 2);
 }
-TEST(LevelStartMenuTest, Wr_Input) {
+TEST(LevelStartMenuTest, Wr_Input)
+{
     ASSERT_EQ(LevelStartMenu(1515), 0);
 }
